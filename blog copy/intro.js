5359aa57361aa5d8ad761f7d7f8958846630db2e
eellda <tree500253@gmail.com>
2022-12-13 16:55:13 +0900

    intro추가01

---
+++ blog copy/intro.js
@@ -0,0 +1,8 @@
+let userBox = document.getElementById('userBox');
+
+function openinfo() {
+  userBox.classList.remove('hide-details');
+}
+function closeinfo() {
+  userBox.classList.add('hide-details');
+}
