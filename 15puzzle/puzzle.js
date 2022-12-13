c7dfc7d8e23e09a80e0384fd0ce6f563735dae63
eellda <tree500253@gmail.com>
2022-12-13 19:03:22 +0900

    15puzzle pj

---
+++ 15puzzle/puzzle.js
@@ -0,0 +1,13 @@
+const winningState = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16];
+
+// var last = document.querySelector(".empty");
+// var ar = Array.from(document.querySelectorAll(".grid-cell"));
+
+const cell = document.getElementsByClassName('grid-cell');
+console.log(cell);
+var onepiece = new Array('')
+
+
+
+// https://codereview.stackexchange.com/questions/219866/15-puzzle-game-javascript 참고
+// https://creator-kyle.tistory.com/26 참고
