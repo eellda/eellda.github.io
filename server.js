4e75d41baefbeb5cfea954e0f17df424ffb23838
eellda <tree500253@gmail.com>
2022-12-20 01:20:30 +0900

    blog - editor add

--- server.js
+++ server.js
@@ -1,13 +1,17 @@
 const express = require('express');
 const path = require('path');
 
-let initial_path = path.join(__dirname, '/');
+let initial_path = path.join(__dirname, '/'); // 서버 디렉토리 위치
 
 const app = express();
 app.use(express.static(initial_path));
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(initial_path, 'home.html'));
+})
+
+app.get('/editor', (req, res) => {
+    res.sendFile(path.join(initial_path, 'editor.html'));
 })
 
 app.listen('3000', () => {
