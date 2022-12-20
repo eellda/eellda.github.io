6fe09d11fa72e47955eeed98cd136afcf1eee2eb
eellda <tree500253@gmail.com>
2022-12-20 18:04:24 +0900

    firebase-is not defined

--- public/js/editor.js
+++ public/js/editor.js
@@ -49,6 +49,36 @@ const addImage = (imagepath, alt) => {
     articleFeild.value = articleFeild.value.slice(0, curPos) + textToInsert + 
     articleFeild.value.slice(curPos);
 }
+
+let months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+
+publishBtn.addEventListener('click', () => {
+    if(articleFeild.value.length && blogTitleField.value.length) {
+        let letters = 'abcdefghijklmnopqrstuvwxyz';
+        let blogTitle = blogTitleField.value.split(" ").join("-");
+        let id = '';
+        for (let i = 0; i < 4; i++) {
+            id += letters[Math.floor(Math.random() * letters.length)];
+        }
+
+        let docName = `${blogTitle}-${id}`;
+        let date = new Date();
+
+        db.collection('blogs').doc(docName).set({
+            title: blogTitleField.value,
+            article: articleFeild.value,
+            bannerImage: bannerPath,
+            publishAt: `${date.getDate()} ${months[date.getMonth()]} ${date.getFullYear()}`
+        })
+        .then(() => {
+            console.log('date entered');
+            //location.href = `/${docName}`;
+        })
+        .catch((err) => {
+            console.error(err);
+        })
+    }
+})
         // https://www.youtube.com/watch?v=AWHGQfzfHxI 30:00
         // npm install xpress-fileupload
 
---
+++ public/js/firebase.js
@@ -0,0 +1,14 @@
+ let firebaseConfig = {
+  apiKey: "AIzaSyDTJ2n_WAXZ-ritIY7AiTaZ8RjfBmne0dw",
+  authDomain: "bolgging-website.firebaseapp.com",
+  projectId: "bolgging-website",
+  storageBucket: "bolgging-website.appspot.com",
+  messagingSenderId: "748753746971",
+  appId: "1:748753746971:web:2a8f6b45e7436ac3bb0cbe"
+};
+
+  // Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+
+let db = firebase.firestore();
